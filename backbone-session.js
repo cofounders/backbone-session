1ef2d5c4a7a287e82616df9828c712ba554a3066
Emile Bergeron <emile.bergeron@solutionstlm.com>
2016-08-19 13:48:28 -0400

    Refactored out ES6 Promise (IE incompatible) in favor of jQuery Promise

--- backbone-session.js
+++ backbone-session.js
@@ -1,4 +1,4 @@
-(function (root, factory) {
+(function(root, factory) {
   if (typeof define === 'function' && define.amd) {
     define(['underscore', 'backbone'], factory);
   } else if (typeof exports === 'object') {
@@ -6,57 +6,59 @@
   } else {
     root.BackboneSession = factory(root._, root.Backbone);
   }
-}(this, function (_, Backbone) {
+}(this, function(_, Backbone) {
   return Backbone.Model.extend({
-    url: function () {
+    url: function() {
       return 'Backbone.Session';
     },
-    initialize: function (properties, options) {
+    initialize: function(properties, options) {
       this.options = options || {};
     },
-    destroy: function (options) {
+    destroy: function(options) {
       return this.sync('delete', this, options);
     },
-    sync: function (method, model, options) {
+    sync: function(method, model, options) {
       options = options || {};
       var url = model.options.url || model.url;
       var key = _.isFunction(url) ? url() : '' + url;
       var response;
       switch (method) {
-      case 'create':
-      case 'update':
-        var data = model.toJSON();
-        var text = JSON.stringify(data);
-        response = localStorage.setItem(key, text);
-        break;
-      case 'delete':
-        response = localStorage.removeItem(key);
-        break;
-      case 'read':
-        response = JSON.parse(localStorage.getItem(key));
-        break;
+        case 'create':
+        case 'update':
+          var data = model.toJSON();
+          var text = JSON.stringify(data);
+          response = localStorage.setItem(key, text);
+          break;
+        case 'delete':
+          response = localStorage.removeItem(key);
+          break;
+        case 'read':
+          response = JSON.parse(localStorage.getItem(key));
+          break;
       }
       if (_.isFunction(options.success)) {
         options.success(response);
       }
-      return Promise.resolve(response);
+      return Backbone.$.Deferred()
+        .resolve(response)
+        .promise();
     },
-    signIn: function (options) {
-      return Promise.reject(Error(
-        'Override the signIn method'
-      ));
+    signIn: function(options) {
+      return Backbone.$.Deferred()
+        .reject(Error('Override the signIn method'))
+        .promise();
     },
-    signOut: function (options) {
+    signOut: function(options) {
       options = options || {};
       this.destroy();
       this.clear();
       this.trigger('signOut');
-      return Promise.resolve();
+      return Backbone.$.Deferred().resolve().promise();
     },
-    getAuthStatus: function (options) {
-      return Promise.reject(Error(
-        'Override the getAuthStatus method'
-      ));
+    getAuthStatus: function(options) {
+      return Backbone.$.Deferred()
+        .reject(Error('Override the getAuthStatus method'))
+        .promise();
     },
     Model: Backbone.Model,
     Collection: Backbone.Collection
